7e0f803c67f93adcd4e6a8267a91354a36a9490a
Ukash25 <125337955+Ukash25@users.noreply.github.com>
2024-10-08 15:08:47 +0530

    Time: 1 ms (67%), Space: 46.7 MB (8.64%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,7 +1,7 @@
 class Solution {
     public int trap(int[] height) {
        int ans=0;
-       int lmax=0,rmax=0;
+       int lmax=-9999,rmax=-9999;
        int l=0;
        int r=height.length-1;
        while(l<r)
