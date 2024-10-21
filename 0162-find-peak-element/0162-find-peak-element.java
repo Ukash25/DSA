9933d135837dc612975ebf7ec5ce9ef336eb1697
Ukash25 <125337955+Ukash25@users.noreply.github.com>
2024-10-21 10:46:42 +0530

    Time: 0 ms (100%), Space: 42 MB (85.37%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,11 +1,10 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-        int left=0;
-        int right=nums.length-1;
-        while(left<right)
-        {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
             int mid=(left+right)/2;
-            if(nums[mid]>nums[mid+1])
+            if (nums[mid] > nums[mid+1])
             {
                 right=mid;
             }
@@ -13,6 +12,6 @@ class Solution {
                 left=mid+1;
             }
         }
-        return left;
+        return right;
     }
 }
