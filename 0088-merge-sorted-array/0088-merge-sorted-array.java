defee9396b8669857070c18b9d0c213c8f6d5eb4
Ukash25 <125337955+Ukash25@users.noreply.github.com>
2024-10-21 14:34:15 +0530

    Time: 1 ms (32.09%), Space: 42 MB (61.76%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,10 +1,12 @@
-class Solution {
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
-    int j=0;
-    for(int i=m;i<m+n;i++)
+class Solution
     {
-        nums1[i]=nums2[j++];
+    public void merge(int[] nums1, int m, int[] nums2, int n)
+     {
+        int j = 0;
+        for (int i = m; i < m + n; i++)
+        {
+            nums1[i] = nums2[j++];
+        }
+        Arrays.sort(nums1);
     }
-    Arrays.sort(nums1);
-    }
-}
+}                                                                                                         
